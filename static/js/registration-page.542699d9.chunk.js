77227f995c593705f7df5523aa58b11d58e9ba67
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-05-19 02:01:54 +0300

    Updates

--- static/js/registration-page.542699d9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[4],{81:function(t,o,e){"use strict";e.r(o);var n=e(1);o.default=function(){return Object(n.jsx)("h1",{children:" \u044d\u0442\u043e \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u0440\u0435\u0433\u0438\u0442\u0441\u0440\u0430\u0446\u0438\u0438"})}}}]);
-//# sourceMappingURL=registration-page.542699d9.chunk.js.map
